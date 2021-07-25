f0472cfce48e448d1a1babbd268b9b832ad39d65
ABelliqueux <contact@arthus.net>
2021-07-25 10:15:43 +0200

    Fix SPU mvol value

--- hello_cdda/hello_cdda.c
+++ hello_cdda/hello_cdda.c
@@ -62,11 +62,13 @@ int main(void)
     // Init Spu
     SpuInit();
     // Set master & CD volume to max
-    spuSettings.mask = (SPU_COMMON_MVOLL | SPU_COMMON_MVOLR | SPU_COMMON_CDVOLL | SPU_COMMON_CDVOLR | SPU_COMMON_CDMIX);
-    spuSettings.mvol.left  = 0x6000;
-    spuSettings.mvol.right = 0x6000;
-    spuSettings.cd.volume.left = 0x6000;
-    spuSettings.cd.volume.right = 0x6000;
+    spuSettings.mask = (SPU_COMMON_MVOLL | SPU_COMMON_MVOLR | SPU_COMMON_CDVOLL | SPU_COMMON_CDVOLR | SPU_COMMON_CDMIX );
+    // Master volume should be in range 0x0000 - 0x3fff
+    spuSettings.mvol.left  = 0x3fff;
+    spuSettings.mvol.right = 0x3fff;
+    // Cd volume should be in range 0x0000 - 0x7fff
+    spuSettings.cd.volume.left = 0x7fff;
+    spuSettings.cd.volume.right = 0x7fff;
     // Enable CD input ON
     spuSettings.cd.mix = SPU_ON;
     // Apply settings
